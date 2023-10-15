8bfbbb8d18ad2a63080057b275dffb904a0961c3
axelpicou <115532798+axelpicou@users.noreply.github.com>
2023-10-15 17:25:00 +0200

    Update projet1.cpp
    final

--- c++/projet1/projet1.cpp
+++ c++/projet1/projet1.cpp
@@ -1,84 +1,107 @@
-// projet1.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <stdlib.h>
 
-class Perso {
+
+
+using namespace std;
+
+class Character {
 public:
-    int vie;
-    int attaque;
-    int agilite;
-    std::string nom;
-
-public:
-
-    Perso(std::string nom_, int vie_, int attaque_, int agilite_);
-    void prendredmg(int dmg) {
-        vie -= dmg;
-        if (vie <= 0) {
-            vie = 0;
-        }
+    Character(string name, int health, int attack) : name(name), health(health), attack(attack) {}
+    void takeDamage(int damage) {
+        health -= damage;
     }
-    void attaquer(Perso& cible) {
-        cible.prendredmg(attaque);
-
+    bool isAlive() {
+        return health > 0;
     }
-    bool vivant() {
-        return vie > 0;
+    int getAttack() {
+        return attack;
+    }
+    string getName() {
+        return name;
+    }
+    int getHealth() {
+        return health;
+    }
+    int generateRandomDamage() {
+        return rand() % (attack / 2) + attack / 2;
     }
 
+private:
+    string name;
+    int health;
+    int attack;
 };
 
+void battle(Character& player, Character& enemy) {
+    int playerDamage = player.generateRandomDamage();
+    int enemyDamage = enemy.generateRandomDamage();
 
-int main(int argc, char const* argv[])
-{
-    Perso perso1("pablo", 100, 10, 10);
-    Perso perso2("miguel", 100, 15, 20);
+    if (player.isAlive() && enemy.isAlive()) {
+        cout << player.getName() << " (Health: " << player.getHealth() << ") attacks " << enemy.getName() << " for " << playerDamage << " damage." << endl;
+        enemy.takeDamage(playerDamage);
+        if (!enemy.isAlive()) {
+            cout <<" GG EZ "<< enemy.getName() << " has been defeated!" << endl;
+            exit(1);
+        }
 
+        cout << enemy.getName() << " (Health: " << enemy.getHealth() << ") attacks " << player.getName() << " for " << enemyDamage << " damage." << endl;
+        player.takeDamage(enemyDamage);
+        if (!player.isAlive()) {
+            cout << " noooo by your fault "<< player.getName() << " has been defeated! poor" << player.getName() << endl;
+            exit(1);
+        }
+    }
+}
 
-    std::cout << "choisisez la dificulté" << std::endl;
-    std::cout << "entre 1 pour facile | 2 pour normal | 3 pour dificile | 4 pour impossible" << std::endl;
-    int dificulte;
-    int mod;
-    std::cin >> dificulte;
-    if (dificulte == 1) {
-        std::cout << "facile" << std::endl;
-        mod = 0,5;
-        Perso perso2("miguel", 100 * mod, 15 * mod, 20 * mod);
-    }
-    else if (dificulte == 2) {
-        std::cout << "normal" << std::endl;
-        mod = 1;
-        Perso perso2("juan", 100 * mod, 15 * mod, 20 * mod);
-    }
-    else if (dificulte == 3) {
-        std::cout << "dificile" << std::endl;
-        mod = 1,5;
-        Perso perso2("benito", 100 * mod, 15 * mod, 20 * mod);
-    }
-    else if (dificulte == 4) {
-        std::cout << "impossible" << std::endl;
-        mod = 3;
-        Perso perso2("juanito", 100 * mod, 15 * mod, 20 * mod);
+void block(Character& player, Character& enemy){
+    int enemyDamage = enemy.generateRandomDamage();
+    int playerBlock = enemyDamage / 2;
+
+    if (player.isAlive() && enemy.isAlive()) {
+        cout << enemy.getName() << " (Health: " << enemy.getHealth() << ") attacks " << player.getName() << " for " << playerBlock << " damage." << endl;
+        player.takeDamage(enemyDamage - playerBlock);
+        if (!player.isAlive()) {
+            cout << " noooo by your fault "<< player.getName() << " has been defeated! poor" << player.getName() << endl;
+            exit(1);
+        }
     }
 
-    std::cout << "votre adversaire est " << perso2.nom << std::endl;
-    std::cout << "votre perso est " << perso1.nom << std::endl;
-    return 0;
 }
 
 
 
+int main() {
+    srand(time(nullptr));
 
+    Character player("jaque", 100, 20);
+    Character enemy("juan", 100, 20);
 
+    cout << "time to fight" << endl;
 
+    while (player.isAlive() && enemy.isAlive()) {
+        cout << "---------------------------------" << endl;
+        cout << player.getName() << " (Health: " << player.getHealth() << ") vs. " << enemy.getName() << " (Health: " << enemy.getHealth() << ")" << endl;
+        cout << "1. Attack" << endl;
+        cout << "2. block" << endl;
+        cout << "Choose your action: ";
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+        int choice;
+        cin >> choice;
 
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+        switch (choice) {
+            case 1:
+                battle(player, enemy);
+                break;
+            case 2:
+                block(player, enemy);
+                break;
+            default:
+                cout << "Invalid choice. Try again." << endl;
+        }
+    }
+    return 0;
+}
